5a5fb885e09608cf007ce4f8ca354d43ff6be95e
felipe gavilan <felipegavilan2202@gamil.com>
2023-10-02 11:45:04 -0500

    se cambia de host

--- index.php
+++ index.php
@@ -5,8 +5,8 @@ date_default_timezone_set("America/Bogota");
 setlocale(LC_TIME, 'es_ES.utf8');
 
 require_once("./vendor/autoload.php");
-define("host", "http://localhost:80");
-//define("host", "http://alfonso.junicalmedical.com.co");
+//define("host", "http://localhost:80");
+define("host", "http://alfonso.junicalmedical.com.co");
 
 use PHP\Controllers\TemplateControlador;
 include_once("config.php");
