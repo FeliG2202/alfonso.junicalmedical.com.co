01d56ef3b3d019ec773334013753c994cd1badd0
felipe gavilan <felipegavilan2202@gamil.com>
2023-11-10 01:28:39 -0500

    se realiza prueba 2

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+
+session_start();
+date_default_timezone_set("America/Bogota");
+setlocale(LC_TIME, 'es_ES.utf8');
+
+require_once("./vendor/autoload.php");
+define("host", "https://localhost");
+//define("host", "http://alfonso.junicalmedical.com.co");
+
+use PHP\Controllers\TemplateControlador;
+include_once("config.php");
+include_once((new TemplateControlador())->cargarTemplate());
