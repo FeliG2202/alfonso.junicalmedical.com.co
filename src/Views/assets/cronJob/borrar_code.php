cb5d6e3e01a657742e8d727fd6ceeca1f7e78fb2
felipe gavilan <felipegavilan2202@gamil.com>
2024-01-29 15:12:31 -0500

    se hace cambio de host

---
+++ src/Views/assets/cronJob/borrar_code.php
@@ -0,0 +1,19 @@
+<?php
+$host = 'db';
+$db   = 'sistema_medico';
+$user = 'root';
+$pass = 'lion';
+$charset = 'utf8mb4';
+
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$opt = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
+$pdo = new PDO($dsn, $user, $pass, $opt);
+
+$sql = "UPDATE personas SET personasCodigo = NULL WHERE TIMESTAMPDIFF(MINUTE, personaDate, NOW()) > 5";
+$stmt = $pdo->prepare($sql);
+$stmt->execute();
+?>
