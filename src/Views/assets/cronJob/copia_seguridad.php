96cb10243ab76521fa25ff6bf7c8accfb310b9f8
felipe gavilan <felipegavilan2202@gamil.com>
2024-01-29 14:36:29 -0500

    modificacion en la base de datos

--- src/Views/assets/cronJob/copia_seguridad.php
+++ src/Views/assets/cronJob/copia_seguridad.php
@@ -11,3 +11,10 @@ $img = $dir . $fecha . "_" . $db . ".sql";
 $command = "mysqldump --opt -h $host -u $user -p $pass $db > $img";
 system($command);
 ?>
+
+
+// 0 0 * * * /usr/bin/php ~/project/Respaldo-MVC/src/Views/assets/cronJob/copia_seguridad.php
+
+// crontab -e para editar archivo
+// crontab -r para eliminar
+// crontab -l para mirar el archivo
